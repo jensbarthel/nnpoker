e757bf3c663089db415edc634984c5a173aa8afb
Jens Barthel <jens.barthel@gmail.com>
2019-12-07 14:01:04 +0100

    Introduce spring boot

--- src/poker/build.gradle.kts
+++ src/poker/build.gradle.kts
@@ -1,11 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val springBootVersion = "2.2.1.RELEASE"
 val junitVersion = "5.5.2"
 val kluentVersion = "1.56"
 val mockkVersion = "1.9.3"
 
+
+
+
 plugins {
     kotlin("jvm") version "1.3.60"
+    kotlin("plugin.spring") version "1.3.60"
+    id("org.springframework.boot") version "2.2.1.RELEASE"
 }
 
 version = "1.0-SNAPSHOT"
@@ -16,6 +22,8 @@ repositories {
 }
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation(platform("org.junit:junit-bom:$junitVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
